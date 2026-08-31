579c5408961e97eef9337ed8ea887c49a1e77faa
lorennam <lorennamenesesdealmeida@gmail.com>
2026-08-31 19:50:13 -0300

    código registros

---
+++ atv2/registro.kt
@@ -0,0 +1,21 @@
+data class Pessoa(val nome: String, val idade: Int, val email: String)
+
+fun main() {
+    val p1 = Pessoa("Lorena", 22, "lorenna@exemplo.com")
+
+    // Acesso nomeado
+    println(p1.nome)
+    println(p1.idade)
+
+    // Desestruturação
+    val (nome, idade, email) = p1
+    println("$nome tem $idade anos")
+
+    // Igualdade por valor (não por referência)
+    val p2 = Pessoa("Lorenna", 22, "lorenna@exemplo.com")
+    println(p1 == p2)
+
+    // Cópia com alteração parcial de campos
+    val p3 = p1.copy(idade = 23)
+    println(p3)
+}
